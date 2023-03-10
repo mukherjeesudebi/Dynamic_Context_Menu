81848740ab8eb2b88460a898a762a62c7ef03de2
mukherjeesudebi <sudebi@vaadin.com>
2023-03-10 13:00:47 +0200

    indentation

--- src/main/java/com/vaadin/components/contextmenuview/TreeGridColumn.java
+++ src/main/java/com/vaadin/components/contextmenuview/TreeGridColumn.java
@@ -10,13 +10,13 @@ import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.treegrid.TreeGrid;
 import com.vaadin.flow.router.Route;
 
-
 @Route("")
 public class TreeGridColumn extends HorizontalLayout {
 
 	private List<Person> managers = PersonService.getPersonList();
+
 	public TreeGridColumn() {
-		
+
 		TreeGrid<Person> treeGrid = new TreeGrid<>();
 		treeGrid.setItems(managers, this::getStaff);
 		treeGrid.addHierarchyColumn(Person::getFirstName).setHeader("First name");
@@ -24,47 +24,45 @@ public class TreeGridColumn extends HorizontalLayout {
 		treeGrid.addColumn(Person::getEmail).setHeader("Email");
 
 		GridContextMenu<Person> menu = treeGrid.addContextMenu();
-        menu.addItem("View", event -> {
-        });
-        menu.addItem("Edit", event -> {
-        });
-        menu.addItem("Delete", event -> {
-        });
-        add(treeGrid);
-        
-        menu.setDynamicContentHandler(person -> {
-            menu.removeAll();
-            menu.addItem(person.getFirstName());
-            menu.addItem(person.getLastName());
-            menu.addItem(person.getId().toString());
-            return true;
-        });
-        
-        
-        Grid<Person> grid = new Grid<>(Person.class, false);
-        grid.addColumn(Person::getFirstName).setHeader("First name");
-        grid.addColumn(Person::getLastName).setHeader("Last name");
-        grid.addColumn(Person::getEmail).setHeader("Email");
-        grid.setItems(PersonService.getPersonList());
-        
-        GridContextMenu<Person> gridMenu = grid.addContextMenu();
-        gridMenu.addItem("View1", event -> {
-        });
-        gridMenu.addItem("Edit1", event -> {
-        });
-        gridMenu.addItem("Delete1", event -> {
-        });
-        add(grid);
-        
-		             
-        gridMenu.setDynamicContentHandler(person -> {
-            gridMenu.removeAll();
-            gridMenu.addItem(person.getFirstName());
-            return true;
-        });
+		menu.addItem("View", event -> {
+		});
+		menu.addItem("Edit", event -> {
+		});
+		menu.addItem("Delete", event -> {
+		});
+		add(treeGrid);
+
+		menu.setDynamicContentHandler(person -> {
+			menu.removeAll();
+			menu.addItem(person.getFirstName());
+			menu.addItem(person.getLastName());
+			menu.addItem(person.getId().toString());
+			return true;
+		});
+
+		Grid<Person> grid = new Grid<>(Person.class, false);
+		grid.addColumn(Person::getFirstName).setHeader("First name");
+		grid.addColumn(Person::getLastName).setHeader("Last name");
+		grid.addColumn(Person::getEmail).setHeader("Email");
+		grid.setItems(PersonService.getPersonList());
+
+		GridContextMenu<Person> gridMenu = grid.addContextMenu();
+		gridMenu.addItem("View1", event -> {
+		});
+		gridMenu.addItem("Edit1", event -> {
+		});
+		gridMenu.addItem("Delete1", event -> {
+		});
+		add(grid);
+
+		gridMenu.setDynamicContentHandler(person -> {
+			gridMenu.removeAll();
+			gridMenu.addItem(person.getFirstName());
+			return true;
+		});
 	}
-	
+
 	public List<Person> getStaff(Person manager) {
-        return PersonService.getPersonListSubTree(manager.getId());
-    }
+		return PersonService.getPersonListSubTree(manager.getId());
+	}
 }
